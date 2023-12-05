ac02f4af01b26faaf1096f4f9cddbd51737aaeb3
Patryk Filip Gryz <pfgryz@gmail.com>
2023-12-05 18:15:28 +0100

    Add basic routing. Add simple error page. Add fluentui to project

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.tsx'
+import App from './routes/app.tsx'
 import './styles/index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import ErrorPage from "./pages/error-page";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        errorElement: <ErrorPage/>,
+        children: []
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router}/>
     </React.StrictMode>,
-)
+);
--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,5 +1,5 @@
 import {render} from "@testing-library/react";
-import App from "../src/app";
+import App from "../src/routes/app";
 
 it("App renders", () => {
     const {getByText} = render(
