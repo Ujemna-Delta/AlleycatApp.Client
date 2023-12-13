b5a8292ff62b0405ca520ececf101e26f1283a4e
Patryk Filip Gryz <pfgryz@gmail.com>
2023-12-13 18:52:31 +0100

    Add HomePage

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import App from "./app.tsx";
 import ErrorPage from "../pages/error-page";
 import AboutPage from "../pages/about-page";
+import HomeRoute from "./home.tsx";
 
 export const routes = [
     {
@@ -8,6 +9,10 @@ export const routes = [
         element: <App/>,
         errorElement: <ErrorPage/>,
         children: [
+            {
+                index: true,
+                element: <HomeRoute/>
+            },
             {
                 path: "/about",
                 element: <AboutPage/>
